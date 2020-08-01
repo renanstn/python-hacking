cc40c6c47df6c324201b489b8f54e9e74cb554bd
Renan Santana <renan_ota@hotmail.com>
2020-08-01 01:45:57 -0300

    ajustes server

--- src/server/server.py
+++ src/server/server.py
@@ -19,13 +19,15 @@ message = "Hello and Welcome".encode()
 client_socket.send(message)
 
 while True:
-    command = input("Digite o comando que deseja executar:")
+    command = input("\nDigite o comando que deseja executar:\n>")
     client_socket.send(command.encode())
     if command.lower() == "exit":
         break
     # Recebe o resultado do comando
-    results = client_socket.recv(BUFFER_SIZE).decode()
-    print(results)
+    print("Aguardando resposta...")
+    response = client_socket.recv(BUFFER_SIZE).decode()
+    print(response)
+
 # Fecha a conexão com o client
 client_socket.close()
 # Fecha a conexão do server
