91eaf7470112f823625a630b63a631230332c7fd
Renan Santana <renan_ota@hotmail.com>
2020-10-11 18:30:35 -0300

    Corige nome do arquivo

--- src/screen_smile/client.py
+++ src/screen_smile/client.py
@@ -8,7 +8,7 @@ SERVER_URL = 'http://localhost:5000/smile'
 
 screenshot = pyautogui.screenshot()
 screenshot.save('./tmp.png')
-with open('./image.png', 'rb') as img_file:
+with open('./tmp.png', 'rb') as img_file:
     payload = {'img': img_file}
     requests.post(SERVER_URL, files=payload)
 os.remove('tmp.png')
