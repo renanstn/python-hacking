b45e8fcb0414d8e0cbda77d7d96427942076572f
Renan Santana <renan_ota@hotmail.com>
2020-11-02 22:44:35 -0300

    Implementa timer no client

--- src/screen_smile/client.py
+++ src/screen_smile/client.py
@@ -1,14 +1,21 @@
 import os
+import time
 import requests
 import pyautogui
 
 
 SERVER_URL = 'http://localhost:5000/smile'
+TIMER = 60
 
 
-screenshot = pyautogui.screenshot()
-screenshot.save('./tmp.png')
-with open('./tmp.png', 'rb') as img_file:
-    payload = {'img': img_file}
-    requests.post(SERVER_URL, files=payload)
-os.remove('tmp.png')
+while True:
+    try:
+        screenshot = pyautogui.screenshot()
+        screenshot.save('./tmp.png')
+        with open('./tmp.png', 'rb') as img_file:
+            payload = {'img': img_file}
+            requests.post(SERVER_URL, files=payload)
+        os.remove('tmp.png')
+        time.sleep(TIMER)
+    except requests.exceptions.ConnectionError:
+        pass
